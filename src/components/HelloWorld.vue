c16a308ca8d29fedabda3ec2eda0c87b575147c1
babu-ch <mashirohira@gmail.com>
2022-09-02 19:04:56 +0900

    clear

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const isFileSelected = ref(false)
+
 const onFileChange = async (e: Event) => {
   const target = e.target as HTMLInputElement
   if (!target.files || !target.files.length) {
     return
   }
+  isFileSelected.value = true
+
   const file = target.files[0]
 
   const url = URL.createObjectURL(file)
@@ -30,11 +36,26 @@ const renderPdf = async (pdf: any) => {
   const renderContext = { canvasContext: context, viewport };
   await page.render(renderContext);
 }
+
+const clear = () => {
+  isFileSelected.value = false
+  const canvas = <HTMLCanvasElement>document.getElementById("canvas")
+  const context = canvas.getContext("2d")
+  context?.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+const download = () => {
+  console.log('ok')
+}
 </script>
 
 <template>
-  <div>
+  <div v-if="!isFileSelected">
     <input type="file" @change="onFileChange">
+  </div>
+  <div v-else>
+    <button @click="clear">clear</button>
+    <button @click="download">download</button>
   </div>
 
   <div id="canvas_container">
