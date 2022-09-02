400c13d990f1a01485019cde7f42aa7191e575b6
babu-ch <mashirohira@gmail.com>
2022-09-02 18:33:42 +0900

    render

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,49 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+const onFileChange = async (e: Event) => {
+  const target = e.target as HTMLInputElement
+  if (!target.files || !target.files.length) {
+    return
+  }
+  const file = target.files[0]
 
-defineProps<{ msg: string }>()
+  const url = URL.createObjectURL(file)
 
-const count = ref(0)
+  const task = await pdfjsLib.getDocument(url)
+
+  const pdf = await task.promise
+
+  await renderPdf(pdf)
+}
+
+const renderPdf = async (pdf: any) => {
+  console.error(pdf)
+  const page = await pdf.getPage(1);
+  const viewport = page.getViewport({scale: 1});
+  console.error(page, viewport)
+
+  const canvas = <HTMLCanvasElement>document.getElementById("canvas")
+  const context = canvas.getContext("2d");
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+
+
+  const renderContext = { canvasContext: context, viewport };
+  await page.render(renderContext);
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    <input type="file" @change="onFileChange">
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div id="canvas_container">
+    <canvas id="canvas"></canvas>
+  </div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+#canvas {
+  width: 500px;
 }
 </style>
